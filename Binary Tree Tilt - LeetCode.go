1f5cff8aee4339ac835d83dfbb5447c75159a14e
yqh231 <yangqihang@guanmai.cn>
2017-11-06 16:29:50 +0800

    binary tree tilt

---
+++ Binary Tree Tilt - LeetCode.go
@@ -0,0 +1,49 @@
+/*
+Binary Tree Tilt - LeetCodeGiven a binary tree, return the tilt of the whole tree.
+
+The tilt of a tree node is defined as the absolute difference between the sum of all left subtree node values and the sum of all right subtree node values. Null node has tilt 0.
+
+The tilt of the whole tree is defined as the sum of all nodes' tilt.
+
+Example:
+
+Input: 
+         1
+       /   \
+      2     3
+Output: 1
+Explanation: 
+Tilt of node 2 : 0
+Tilt of node 3 : 0
+Tilt of node 1 : |2-3| = 1
+Tilt of binary tree : 0 + 0 + 1 = 1
+
+
+
+Note:
+
+The sum of node values in any subtree won't exceed the range of 32-bit integer. 
+All the tilt values won't exceed the range of 32-bit integer.
+
+*/
+package main
+
+func findTilt(root *TreeNode) int {
+  var str = 0
+  SumNode(root, &str)
+  return str
+  
+}
+
+func SumNode(root *TreeNode, result *int) int{
+  if root == nil{
+    return 0
+  }
+  l, r := SumNode(root.Left, result), SumNode(root.Right, result)
+  diff := l - r
+  if diff <0{
+    diff = -diff
+  }
+  *result += diff
+  return root.Val + l + r
+}
